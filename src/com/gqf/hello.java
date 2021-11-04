6de12d7217dd99667496aa41c7ca3ee6abd0135e
gqf <1138153570@qq.com>
2021-11-04 12:47:12 +0800

    更新

---
+++ src/com/gqf/hello.java
@@ -0,0 +1,13 @@
+package com.gqf;
+
+/**
+ * @ClassName hello
+ * @Description TODO
+ * @Author gqfeng
+ * @Date 2021/11/4
+ **/
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+}
